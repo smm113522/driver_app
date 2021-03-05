83c4cb61fa6d18ea0247f02235f9d07fc6584b0f
乐观开发者 <908085643@qq.com>
2021-03-05 15:52:34 +0800

    init app

---
+++ src/test/java/com/driver/app/driver_app/DriverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driver.app.driver_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
